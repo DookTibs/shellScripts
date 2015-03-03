00067da431a85b70bd2ccadf3856d575c83863c6
Thomas Feiler <tfeiler@carleton.edu>
2015-03-03 16:36:52 -0600

    made tag generation more generic

--- regenReasonTags.sh
+++ regenReasonTags.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+if [ -z "${1}" ]; then
+	area="ventnorTfeilerReason"
+else
+	area="${1}";
+fi
+
 # can't get find/ctags to work quite right over symbolic links like we have in a reason install...
 # cd ~/remotes/wsgTfeilerReason/html
 # find -L . -name "*.php" | ctags -f phpTags -L -
@@ -9,9 +15,9 @@ date
 # cd ~/remotes/wsgTfeilerReasonCore/
 # find reason_package_20140404 -name "*.php" | ctags -f phpTags -L -
 
-echo "generating tags for ventnor slote-apps reason installation"
-cd ~/remotes/ventnorTfeilerReason/
-# cd ~/remotes/ventnorMryanReason/
+echo "generating tags for ventnor reason installation mounted on $area"
+# cd ~/remotes/ventnorTfeilerReason/
+cd ~/remotes/${area}/
 find . -name "*.php" | ctags -f phpTags -L -
 
 date
