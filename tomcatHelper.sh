5a73e4e82d4ef3dfb682219ac0d63bd35bfaf2f7
Tom Feiler <tfeiler@gmail.com>
2017-02-15 13:15:14 -0500

    flesh out tomcatHelper to be smarter about delays and duplicated code

--- tomcatHelper.sh
+++ tomcatHelper.sh
@@ -6,6 +6,9 @@ if [ "cygwin" != "${TOM_OS}" ];then
 	exit 1
 fi
 
+TOMCAT_HOME=/cygdrive/c/development/tomcat/apache-tomcat-9.0.0.M15/
+logfile=${TOMCAT_HOME}logs/catalina.out
+
 runTomcatCmd() {
 	if [ "${1}" == "stop" ] || [ "${1}" == "start" ]; then
 		CLASSPATH="/cygdrive/c/Program\ Files/Java/jdk1.8.0_112/lib/tools.jar" CATALINA_OPTS="-Dspring.profiles.active=dev,migration -DbaseUrl=http://localhost:8080 -Djava.endorsed.dirs=/cygdrive/c/development/tomcat/apache-tomcat-9.0.0.M15/endorsed -XX:+CMSClassUnloadingEnabled -Dfile.encoding=Cp1252" JAVA_HOME="/cygdrive/c/Program Files/Java/jdk1.8.0_112/" bash -c "/cygdrive/c/development/tomcat/apache-tomcat-9.0.0.M15/bin/catalina.sh $1"
@@ -14,26 +17,76 @@ runTomcatCmd() {
 	fi
 }
 
-usage() {
-	if [ "${1}" != "" ]; then
-		echo "Unsupported arg '${1}'"
-	else
-		echo "No arg supplied"
-	fi
-	echo "Supported args: 'status', 'stop', 'start', 'watch'"
+startTomcat() {
+	echo "Starting Tomcat"
+	runTomcatCmd start
+	echo "Waiting for webapp initialization to complete..."
+	startLine=`logwatcher.sh $logfile "Server startup in"`
+	msToStart=`echo "${startLine}" | awk '{ print $(NF-1) }'`
+	prettyTime $msToStart
+	echo "Initialization completed in ${_prettyTime}"
 }
 
-if [ "cygwin" = ${TOM_OS} ];then
-	processId=`procps all | grep "apache-tomcat-9.0.0.M15" | grep "\-DbaseUrl=.*localhost:8080" | awk '{ print $3 }'`
-fi
+processId=""
+# runs and sets global var processId
+getProcessId() {
+	if [ "cygwin" = ${TOM_OS} ];then
+		processId=`procps all | grep "apache-tomcat-9.0.0.M15" | grep "\-DbaseUrl=.*localhost:8080" | awk '{ print $3 }'`
+	fi
+}
+
+usage() {
+	if [ "${1}" != "" ]; then
+		echo "Unsupported command '${1}'"
+	else
+		echo "No command supplied"
+	fi
+	echo "Supported commands:"
+	echo "'status'	Check if Tomcat is running or not"
+	echo "'stop'		Stop Tomcat localhost instance using scripts"
+	echo "'kill'		Stop Tomcat localhost instance using shell kill command"
+	echo "'start'		Stop Tomcat localhost instance"
+	echo "'redeploy'	Builds and deploys DRAGON Online war, bouncing the Tomcat localhost instance"
+	echo "'watch'		Tails the Tomcat log file"
+}
+
+_prettyTime=""
+prettyTime() {
+	ms=${1}
+	seconds=$(expr ${ms} / 1000)
+	minutes=$(expr ${seconds} / 60)
+	leftoverSeconds=$(expr ${seconds} % 60)
+	leftoverMs=$(expr ${ms} % 1000)
+	_prettyTime="${minutes}:${leftoverSeconds}.${leftoverMs}"
+}
+
+waitForStoppage() {
+	# keep checking til processId is dead
+	while [ 1 -eq 1 ]; do
+		echo "waiting for tomcat to stop..."
+		getProcessId
+		if [ -z "${processId}" ]; then
+			break
+		fi
+		sleep 1
+	done
+	echo "Tomcat stopped!"
+}
+
+# get the process id
+getProcessId
 
 if [ -z $1 ]; then
 	usage
-elif [ "${1}" == "stop" ] || [ "${1}" == "status" ]; then
+elif [ "${1}" == "stop" ] || [ "${1}" == "status" ] || [ "${1}" == "kill" ]; then
 	if [ -n "${processId}" ]; then
 		echo "Tomcat is running as pid ${processId}"
 		if [ "${1}" == "stop" ]; then
 			runTomcatCmd stop
+			waitForStoppage
+		elif [ "${1}" == "kill" ]; then
+			echo "Killing..."
+			kill ${processId}
 		fi
 	else
 		echo "Tomcat does not appear to be running."
@@ -42,12 +95,31 @@ elif [ "${1}" == "start" ]; then
 	if [ -n "${processId}" ]; then
 		echo "Tomcat is already running as pid ${processId}"
 	else
-		runTomcatCmd start
+		startTomcat
 	fi
 elif [ "${1}" == "redeploy" ]; then
-	echo "Not yet implemented; clear out webapps/ROOT/, make sure dragon.war is up to date, etc."
+	echo "Rebuilding war..."
+	cd /cygdrive/c/Users/38593/workspace/icf_dragon
+	mvn package
+
+	if [ $? -ne 0 ]; then
+		echo "Error building war; not proceeding."
+		exit 1
+	fi
+
+	if [ -n "${processId}" ]; then
+		echo "Stopping running Tomcat process..."
+		runTomcatCmd stop
+		waitForStoppage
+	fi
+
+	echo "Clearing out installed webapp from Tomcat..."
+	rm -rf /cygdrive/c/development/tomcat/apache-tomcat-9.0.0.M15/webapps/ROOT/
+	cp target/dragon-0.0.1-SNAPSHOT.war /cygdrive/c/development/tomcat/apache-tomcat-9.0.0.M15/webapps2
+
+	startTomcat
 elif [ "${1}" == "watch" ]; then
-	tail -f /cygdrive/c/development/tomcat/apache-tomcat-9.0.0.M15/logs/catalina.out
+	tail -F ${logfile}
 else
 	usage "${1}"
 fi
