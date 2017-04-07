c4588ae6ae112549e87c90809f8d325e5c435873
Tom Feiler <tfeiler@gmail.com>
2017-04-07 10:10:03 -0400

    modify dragon searcher to use sag

--- findDragonJava.sh
+++ findDragonJava.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 cd /cygdrive/c/Users/38593/workspace/icf_dragon/src/main/java/
-find . -name "*.java" -exec grep -l "$1" {} \;
-# ag -li "$1"
-cd -
+# find . -name "*.java" -exec grep -l "$1" {} \;
+# ag -l "$1"
+
+# let's use sag
+# run this either as:
+#
+#	# just lists result files
+#	findDragonJava "SomeSearch"
+#
+#	# outputs line contents (-v == 'verbose')
+#	findDragonJava "SomeSearch" -v
+
+justList=1
+if [ ! -z "${2}" ]; then
+	if [ "${2}" == "-v" ]; then
+		justList=0
+	fi
+fi
+
+if [ ${justList} -eq 1 ]; then
+	sag -l "$1" | sed 's/cygdrive.*src\/main\/java\///'
+else
+	sag "$1" | sed 's/cygdrive.*src\/main\/java\///'
+fi
