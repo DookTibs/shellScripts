502d6264bba051328b6bac2651f2e8391fc594cb
Tom Feiler <tom.feiler@icf.com>
2026-01-16 15:52:43 -0600

    homegrown vim debugger experiments, continued

--- pdb_vim_bridge.py
+++ pdb_vim_bridge.py
@@ -2,11 +2,7 @@
 
 import time, sys, re, json, socket, threading, socketserver
 
-# class that watches a debugger log (jdb only supported one now) and 
-# fires off related updates to a connected Vim channel. See also
-# ~/development/configurations/vim/tibs_jdb.vim. Right now this type of
-# setup only supports a few options but I hope to build it out slowly
-# over time so I can stay in tmux even when debugging.
+# see jdb_vim_bridge. This is a version for pdb (Python debugger). If this works too, maybe redesign to use either?
 
 # line 1 used to be:
 #!/usr/bin/python
@@ -53,7 +49,7 @@ class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     pass
 
-# takes raw output from JDB and parses it out a bit to make it easier for Vim to handle
+# takes raw output from PDB and parses it out a bit to make it easier for Vim to handle
 def createPayload(batch):
     payload = None
     if len(batch) > 0:
@@ -65,13 +61,24 @@ def createPayload(batch):
         # compile these once outside the function. Maybe rewrite whole thing as a 
         # class at some point; would let me add support for other debuggers
         # that way
-        listBreakpointsPattern = re.compile("Breakpoints set:")
-        addedBreakpointPattern = re.compile("Set breakpoint (.*):(.*)")
-        removedBreakpointPattern = re.compile("Removed: breakpoint (.*):(.*)")
-        stoppedPattern = re.compile("(Breakpoint hit|Step completed): \"(.*)\", (.*), line=(.*) bci=(.*)")
+        listBreakpointsPattern = re.compile("^Num.*Type.*Disp.*Enb.*Where")
+        addedBreakpointPattern = re.compile("^Breakpoint (\\d*) at (.*):(.*)")
+        removedBreakpointPattern = re.compile("^Deleted breakpoint (.*) at (.*):(.*)")
+        # stoppedPattern = re.compile("(Breakpoint hit|Step completed): \"(.*)\", (.*), line=(.*) bci=(.*)")
+        # stoppedPattern = re.compile("^> (.*)\\((.*)\\)(<module>|.*())(->\\d*)")
+        stoppedPattern = re.compile(r"^> (.*)\((\d+)\)(<module>|.*)\(\)(->(.*))?")
 
+        # > /Users/38593/development/samplePythonDebug/hello.py(16)<module>()
+        # > /Users/38593/development/samplePythonDebug/hello.py(9)main()
+        # > /Users/38593/development/samplePythonDebug/hello.py(1)squareit()
+        # > /Users/38593/development/samplePythonDebug/hello.py(2)squareit()
+        # > /Users/38593/development/samplePythonDebug/hello.py(2)squareit()->25
+        # > /Users/38593/development/samplePythonDebug/hello.py(10)main()
+
+        """
         if (batch[0] == "> "):
             batch = batch[1:]
+        """
 
         firstLine = batch[0]
         print(f"firstLine: [{firstLine}]")
@@ -81,46 +88,59 @@ def createPayload(batch):
                 print(f"\t[{i}]: {batch[i]}")
 
         if (listBreakpointsPattern.match(firstLine)):
+            """
             bps = [
                 # hardcoded test data
                 { "class": "com.icfi.dragon.web.controller.AssessmentLiteratureManagementController", "line": 1 },
                 { "class": "com.icfi.dragon.web.controller.AssessmentLiteratureManagementController", "line": 2 }
             ]
+            """
+            bps = []
+            for i in range(1, len(batch)):
+                chunks = batch[i].split(" ")[-1].split(":")
+                bps.append({ "file": chunks[0], "line": int(chunks[1])})
+
             payload = {
                 "type": "breakpoint_list",
                 "breakpoints": bps
             }
         elif (addedBreakpointPattern.match(firstLine)):
             lazy = addedBreakpointPattern.match(firstLine)
-            bp = { "class": lazy.group(1), "line": lazy.group(2) }
+            bp = { "file": lazy.group(2), "line": int(lazy.group(3)) }
             payload = {
                 "type": "breakpoint_added",
                 "breakpoint": bp
             }
         elif (removedBreakpointPattern.match(firstLine)):
             lazy = removedBreakpointPattern.match(firstLine)
-            bp = { "class": lazy.group(1), "line": lazy.group(2) }
+            bp = { "file": lazy.group(2), "line": int(lazy.group(3)) }
             payload = {
                 "type": "breakpoint_removed",
                 "breakpoint": bp
             }
-        elif (stoppedPattern.match(firstLine)):
-            lazy = stoppedPattern.match(firstLine)
-            fxn = lazy.group(3)
+        else:
+            for line in batch:
+                if (stoppedPattern.match(line)):
+                    print(f"STOPPED PATTER HIT!\n" * 5)
+                    lazy = stoppedPattern.match(line)
+                    stopped_file_name = lazy.group(1)
+                    stopped_file_line = lazy.group(2)
+                    stopped_context = lazy.group(3)
+                    # d = lazy.group(4)
+                    stopped_rv = lazy.group(5)
 
-            # this MIGHT be the path to the file; Vimscript can try to open it up
-            pathGuess = fxn.replace(".", "/")
-            pathGuess = "/".join(pathGuess.split("/")[0:-1]) + ".java"
-
-            payload = {
-                "type": "execution_paused",
-                "subtype": lazy.group(1).lower(),
-                "context": lazy.group(2),
-                "possiblePartialFilePath": pathGuess,
-                "function": fxn,
-                "lineNumber": int(lazy.group(4).replace(",","")),
-                "byteCodeIndex": lazy.group(5),
-            }
+                    payload = {
+                        "type": "execution_paused",
+                        # "subtype": "n/a",
+                        "context": stopped_context,
+                        # "possiblePartialFilePath": stopped_file_name,
+                        # "function": "n/a",
+                        "actualCompleteFilePath": stopped_file_name,
+                        "lineNumber": int(stopped_file_line),
+                        "rv": stopped_rv
+                        # "byteCodeIndex": "n/a"
+                    }
+                    break
 
 
     if payload == None:
@@ -166,15 +186,13 @@ if __name__ == "__main__":
     # prints a prompt again. We can use this to determine end of output and buffer it up
     # and send it to Vim in a single JSON call (hopefully!). If we're in the middle of a run,
     # the prompt changes.
-    endOfOutput = ">"
-    # altEndOfOutput = "http-nio"
-    altEndOfOutput = r"^main\[\d*\]"
+    endOfOutput = "(Pdb)"
+    altEndOfOutput = "http-nio"
     altRegex = re.compile(altEndOfOutput)
 
     bufferedOutput = []
     for l in follow(f):
         l = l[:-1]
-        print(f"CHECK ON '{l}' vs '{endOfOutput}' / '{altEndOfOutput}'")
         if (l == endOfOutput or altRegex.match(l) ):
             bufferedOutput = [line.replace("\t", "").replace("\r", "") for line in bufferedOutput]
             bufferedOutput = list(filter(lambda x: x.strip() != "", bufferedOutput)) # strip empty lines
